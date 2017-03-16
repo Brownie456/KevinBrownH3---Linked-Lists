53fda3adb9dada3219562ac7ca01ed1c8764a12a
Brownie456 <kbrown23@albany.edu>
2017-03-15 21:56:32 -0400

    adjusted findNode and addNode
    addnode now tracks tail.
    findNode now implemented.

--- src/DoublyLinked.java
+++ src/DoublyLinked.java
@@ -1,7 +1,7 @@
 
 public class DoublyLinked {
 
-    private Node head;
+    private static Node head;
     private Node tail;
     private int size;
 
@@ -16,10 +16,10 @@ public class DoublyLinked {
     public void add(String data) {
         Node newNode = new Node(data, null, null);
         Node temp, holder;
-        boolean state = false;
 //if the list is empty put item into the list
         if (head == null) {
             head = newNode;
+            tail = head;
         }
         //if the data is higher alphabetically, put it in front
         else if (data.compareTo(head.getData()) <= 0) {
@@ -37,12 +37,13 @@ public class DoublyLinked {
                     newNode.setPrev(temp);
                     newNode.setNext(holder);
                     holder.setPrev(newNode);
-                    state = true;
                     break;
                 }
                 //if data has reached end of the list then put data here
                 else if (holder.getNext() == null) {
                     holder.setNext(newNode);
+                    newNode.setPrev(holder);
+                    tail = newNode;
                     break;
                 }
                 //iterates through the list
@@ -52,64 +53,116 @@ public class DoublyLinked {
                 }
             }
         }
-            System.out.println(newNode);
-            size++;
+
+        size++;
         }
 
 
 
 
-    public void findNode(){
+    public static Node sToNode(String find) {
+        Node temp = head;
+        Node holder = temp.getNext();
+            while (holder != null) {
+                if (find.compareTo(temp.getData()) == 0) {
+                    return temp;
+                } else {
+                    temp = holder;
+                    holder = holder.getNext();
+                }
+            }
+            return null;
+        }
 
-    }
+
+
+
 //Traverse the list forward and print
     public void forwardTrav() {
         Node temp = head;
+        System.out.print("Iterate Forward <-> ");
         while (temp != null) {
-            System.out.println(temp.getData());
+            System.out.print(temp.getData()+ " <-> ");
             temp = temp.getNext();
         }
     }
+    //iterate the list backwards and print
     public void backTrav(){
         Node temp = tail;
+        System.out.print("Iterate backward <-> ");
         while (temp != null) {
-            System.out.println(temp.getData());
+            System.out.print(temp.getData()+ " <-> ");
             temp = temp.getPrev();
         }
 
     }
 
-    public void deleteNode(){
-
+    public void deleteNode(Node data) {
+        Node temp = head;
+        Node next = temp.getNext();
+        if (this.size == 1) {
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            System.out.println("1");
+            System.out.println("deleted node:"+data);
+            return;
+        }
+            if (data.getPrev() == null){
+                head = head.getNext();
+                size--;
+                System.out.println("2");
+                System.out.println("deleted node:"+data);
+                return;
+            }
+            if (data.getNext() == null){
+                tail = tail.getPrev();
+                size--;
+                System.out.println("3");
+                System.out.println("deleted node:"+data);
+                return;
+            }
+            while (next != null){
+            if (data.getData().compareTo(temp.getData()) == 0) {
+                temp.getPrev().setNext(temp.getNext());
+                temp.getNext().setPrev(temp.getPrev());
+                System.out.println("4");
+                System.out.println("deleted node:"+data);
+                size--;
+                return;
+            }
+            else {
+                temp = next;
+                next = next.getNext();
+            }
+        }
+        System.out.println("Does not match any node");
+            return;
     }
+
+
 
     public void deleteList(){
 
     }
 
-    public void display()
-    {
-        System.out.print("Alphabetically sorted: ");
-        if (size == 0)
-        {
-            System.out.print("empty\n");
-            return;
+
+    public void findNode(String find) {
+        Node temp = head;
+        Node holder = temp.getNext();
+        while (holder != null) {
+            if (find.compareTo(temp.getData()) == 0) {
+                System.out.println("here is your node: " + temp);
+                return;
+            } else {
+                temp = holder;
+                holder = holder.getNext();
+            }
         }
-        if (head.getNext() == null)
-        {
-            System.out.println(head.getData() );
-            return;
-        }
-        Node ptr = head;
-        System.out.print(head.getData()+ " , ");
-        ptr = head.getNext();
-        while (ptr.getNext() != null)
-        {
-            System.out.print(ptr.getData()+ " , ");
-            ptr = ptr.getNext();
-        }
-        System.out.println(ptr.getData());
+        System.out.println("Does not match any node");
+        return;
     }
+
 }
 
 
